bd6395536b3ba09abffbf83d66dfdd2f29b6d370
Hassnain <hassnainnizam99@gmail.com>
2026-01-02 19:45:24 +0500

    Changed the title

--- mainhtml.php
+++ mainhtml.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>MediPulse OPD</title> 
+    <title>MediPulse OPD Web</title> 
     <link rel="stylesheet" href="website.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
